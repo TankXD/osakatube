3a313c5adb2010dc642d8bb62fa3de4587ae0de6
TankXD <tjwcskdle1@gmail.com>
2022-10-11 03:34:46 +0900

    UPDATE User Create Part - Password bcrypt hash

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -6,6 +7,10 @@ const userSchema = new mongoose.Schema({
   password: { type: String, require: true },
   name: { type: String, require: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const user = mongoose.model("user", userSchema);
