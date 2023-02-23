9b255aea3c727aa329b59e8a45b59063566217b2
TankXD <91671206+TankXD@users.noreply.github.com>
2023-02-24 00:58:55 +0900

    Delete build directory

--- build/frontend/js/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _regeneratorRuntime = _interopRequireDefault(require("regenerator-runtime"));
-require("../scss/styles.scss");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
